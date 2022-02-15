93dbc309a1cac4c59d5b8a79aa766667c5d48784
Keegan Messeur <kmesseur@gmail.com>
2022-02-15 11:42:03 +0200

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheadingComponent } from './topheading/topheading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadingComponent
   ],
   imports: [
     BrowserModule,
